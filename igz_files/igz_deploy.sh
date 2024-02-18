6be06826e98298ebf2439bb48c40ff4d9a5f5da5
alexpirogovski <pirogovski@gmail.com>
2024-02-18 19:20:19 +0200

    WIP

--- igz_files/igz_deploy.sh
+++ igz_files/igz_deploy.sh
@@ -54,8 +54,13 @@ echo "==> Extract Kubespray"
 #chown -R iguazio:iguazio .
 
 echo "==> Install python3.9 on controller"
-echo $SUDO_PASS | sudo -S rpm -ivh rpms/python39-3.9.16-standalone.el7.x86_64.rpm
-echo $SUDO_PASS | sudo -S cp push.py /usr/local/bin/
+if ! command -v python3.9 &> /dev/null; then
+  echo $SUDO_PASS | sudo -S rpm -ivh rpms/python39-3.9.16-standalone.el7.x86_64.rpm
+fi
+
+echo "==> Copy pushing script"
+chmod +x igz_push.py
+echo $SUDO_PASS | sudo -S cp igz_push.py /usr/local/bin/
 
 echo "==> Create venv and install requirements"
 python3.9 -m venv venv
